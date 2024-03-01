612967c5e268735d39e6bafe80d69aebfa3a6899
Jackson Cassemiro <jacksondgls@live.com>
2024-03-01 09:20:36 -0300

    Estrutura das página, inserção de menu e atualização do botão de navegação a por "Link"
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Home } from './components/Home/Home'
+import { About } from './components/About/About'
+import { Menu } from './components/Menu/Menu'
+import './styles/global.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
+
